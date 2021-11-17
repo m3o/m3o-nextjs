b9135ae81d0c7fb4cabdb6f106be141404e54589
Martin Virando <martinvirando@hotmail.co.uk>
2021-11-17 12:30:52 +0000

    Added the password reset handlers on the server

--- src/user/server/handle-auth/index.ts
+++ src/user/server/handle-auth/index.ts
@@ -1,46 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import type { HandleAuthOpts } from './types'
 import { sendError } from '../../../utils/errors'
 import { loginUser } from './routes/login'
 import { signUp } from './routes/signUp'
 import { me } from './routes/me'
 import { logoutUser } from './routes/logoutUser'
-import { changePassword } from './routes/changePassword'
-import { verify } from './routes/verify'
+import { resetPassword } from './routes/resetPassword'
+import { sendPasswordResetEmail } from './routes/sendPasswordResetEmail'
 
 enum Methods {
   Get = 'GET',
   Post = 'POST'
 }
 
-export function handleAuth() {
+const HANDLERS = {
+  [`${Methods.Post} login`]: loginUser,
+  [`${Methods.Post} logout`]: logoutUser,
+  [`${Methods.Post} sign-up`]: signUp,
+  [`${Methods.Get} me`]: me,
+  [`${Methods.Post} reset-password`]: resetPassword,
+  [`${Methods.Post} send-password-reset-email`]: sendPasswordResetEmail
+}
+
+export function handleAuth(opts: HandleAuthOpts = {}) {
   return (req: NextApiRequest, res: NextApiResponse) => {
     const { method, query } = req
     const route = query.m3oUser as string
 
-    if (route === 'login' && method === Methods.Post) {
-      return loginUser(req, res)
+    const handler = HANDLERS[`${method} ${route}`]
+
+    if (!handler) {
+      return sendError({ message: 'Method not allowed', res, statusCode: 405 })
     }
 
-    if (route === 'logout' && method === Methods.Post) {
-      return logoutUser(req, res)
-    }
-
-    if (route === 'sign-up' && method === Methods.Post) {
-      return signUp(req, res)
-    }
-
-    if (route === 'me' && method === Methods.Get) {
-      return me(req, res)
-    }
-
-    if (route === 'change-password' && method === Methods.Post) {
-      return changePassword(req, res)
-    }
-
-    if (route === 'verify' && method === Methods.Post) {
-      return verify(req, res)
-    }
-
-    return sendError({ message: 'Method not allowed', res, statusCode: 405 })
+    return handler(req, res, opts)
   }
 }
